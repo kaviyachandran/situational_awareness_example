47eeaacedeae33e13ab76d3156b0f761e640ed75
kaviyachandran <kaviya.dhanabalachandran@gmail.com>
2021-08-27 11:14:53 +0200

    WIP - Add predicate to get tasks

--- src/cutting.pl
+++ src/cutting.pl
@@ -12,6 +12,11 @@ queries to think about :
 - Preconditions - State of the situation
 - what is the missing object for the task
 - will I able to do the task given the objects
+
+Sol:
+1. Get the dispositions of the objects
+2. Get the task afforded by these dispositions
+3. Find the common tasks when there is more than one object
 */
 
 assert_objects_in_scene([ObjectType | Rest]) :-
@@ -19,7 +24,7 @@ assert_objects_in_scene([ObjectType | Rest]) :-
     tell( [ is_physical_object(Object),
         has_type(Object, ObjectType)]),
     assert_objects_in_scene(Rest),
-    has_disposition(ObjectType, Disposition).
+    get_task_for_objects_(ObjectType, Task).
     
     /* subclass_of(sa:'Cucumber', C), 
     has_description(C, exactly(soma:hasDisposition, 1, C1)), 
@@ -30,5 +35,21 @@ assert_objects_in_scene([ObjectType | Rest]) :-
     %to get task - subclass_of(sa:'Splitting', Desc), has_description(Desc, only(soma:affordsTask, Task)).%
     
 assert_objects_in_scene([]) :- writeln('nothing').
+
+get_task_for_objects_(ObjectType, Task) :-
+    get_dispositions_(ObjectType, Dispositions),
+
+    subclass_of(Disposition, Desc1), 
+    has_description(Desc1, only(soma:affordsTask, Task)).
+
+get_dispositions_(ObjectType, Disposition) :-
+    ((subclass_of(ObjectType, Desc), 
+    has_description(Desc, exactly(soma:hasDisposition, 1, Disposition)));
+    (transitive(subclass_of(ObjectType, Desc)), 
+    has_description(Desc, only(dul:hasRole, Role)),
+    is_restriction(R1),
+    is_restriction(R1, only(soma:affordsTrigger, Role)), 
+    transitive(subclass_of(Disposition, R1))
+    )).
 
 %create_affordances()
